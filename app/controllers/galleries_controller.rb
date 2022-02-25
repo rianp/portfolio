57fd6cf7c7e0cec9a1525dcb82d19eeaf377fb40
Ryan Pickell <rianchristopher@gmail.com>
2022-02-25 00:57:03 -0800

    Integrated new and create actions for galleries

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -2,4 +2,24 @@ class GalleriesController < ApplicationController
     def index
         @gallery_items = Gallery.all 
     end
+
+    def new
+        @gallery_item = Gallery.new
+    end
+
+    # POST /galleries
+def create
+    @gallery = Gallery.new(params.require(:gallery).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @gallery.save
+        format.html { redirect_to galleries_path, notice: "gallery was successfully created." }
+        format.json { render :show, status: :created, location: @gallery }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @gallery.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
 end
